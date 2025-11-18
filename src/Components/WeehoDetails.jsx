fd2faf8a74f401557386d7c807f161f110ee87f5
Akansha2006 <akankshanagar2006@gmail.com>
2025-11-18 19:10:20 +0530

    update files

--- src/Components/WeehoDetails.jsx
+++ src/Components/WeehoDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import AboutPage from "../pages/About";
 import PopularEvent1 from "./PopularEvent1";
 import PopularEvent2 from "./PopularEvent2";
 import ObjectivesSection from "./ObjectivesSection";
@@ -13,6 +14,7 @@ export default function WeehoDetails() {
   return (
     <div className="weeho-page">
       <Navbar/>
+      <AboutPage/>
       
 
       <ObjectivesSection />
